2cdc5248d943e636fbc56e7c04f0da5d7c12253b
JC-Choo <jicheolchoo@gmail.com>
2021-01-09 21:24:02 +0900

    test

--- algorithm/src/main/java/dev/chu/algorithm/programmers/SortKotlin.kt
+++ algorithm/src/main/java/dev/chu/algorithm/programmers/SortKotlin.kt
@@ -7,7 +7,6 @@ class SortKotlin {
 //        val arr2 = arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3))
 //        Level_1_K번째수(arr, arr2)
 
-<<<<<<< HEAD
 //        val numbers00 = intArrayOf(6, 10, 2)
 //        val numbers01 = intArrayOf(3, 30, 34, 5, 9)
 //        val numbers02 = intArrayOf(121, 12)
@@ -17,12 +16,12 @@ class SortKotlin {
 //        println(Level_2_가장_큰_수(numbers02))
 //        println(Level_2_가장_큰_수(numbers03))
 
-        val citations00 = intArrayOf(3, 0, 6, 1, 5)
-        val citations01 = intArrayOf(10, 8, 5, 4, 3)
-        val citations02 = intArrayOf(4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6)
-        val citations03 = intArrayOf(10, 50, 100)
-        val citations04 = intArrayOf(2, 2, 2)
-        val citations05 = intArrayOf(10, 100)
+        val citations00 = intArrayOf(3, 0, 6, 1, 5) // 3
+        val citations01 = intArrayOf(10, 8, 5, 4, 3)    // 4
+        val citations02 = intArrayOf(4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6) // 6
+        val citations03 = intArrayOf(10, 50, 100)   // 3 ???
+        val citations04 = intArrayOf(2, 2, 2) // 2 ???
+        val citations05 = intArrayOf(10, 100) // 2 ???
         println("3 = "+Level2_H_index(citations00))
         println("4 = "+Level2_H_index(citations01))
         println("6 = "+Level2_H_index(citations02))
@@ -56,20 +55,12 @@ class SortKotlin {
         }
 
         return answer
-=======
-
-        val numbers00 = intArrayOf(6, 10, 2)
-        val numbers01 = intArrayOf(3, 30, 34, 5, 9)
-        println(Level_2_가장_큰_수(numbers00))
-        println(Level_2_가장_큰_수(numbers01))
->>>>>>> c442bba... Add sort algorithm
     }
 
     private fun Level_1_K번째수(array: IntArray, commands: Array<IntArray>): IntArray {
         var answer = intArrayOf()
 
         // 풀이 1
-<<<<<<< HEAD
         val list = mutableListOf<Int>()
         commands.forEach { intArr ->
             val i = intArr[0]
@@ -81,19 +72,6 @@ class SortKotlin {
             list.add(arr[k-1])
         }
         answer = list.toIntArray()
-=======
-//        val list = mutableListOf<Int>()
-//        commands.forEach { intArr ->
-//            val i = intArr[0]
-//            val j = intArr[1]
-//            val k = intArr[2]
-//
-//            val arr = array.copyOfRange(i-1, j)
-//            arr.sort()
-//            list.add(arr[k-1])
-//        }
-//        answer = list.toIntArray()
->>>>>>> c442bba... Add sort algorithm
 
         // 풀이 2
         answer = commands.map { intArr ->
@@ -102,33 +80,4 @@ class SortKotlin {
 
         return answer
     }
-<<<<<<< HEAD
-=======
-
-    private fun Level_2_가장_큰_수(numbers: IntArray): String {
-        var answer = ""
-
-        val list = mutableListOf<String>()
-        numbers.forEachIndexed { index, i ->
-            if (i == 0) return@forEachIndexed
-            var str = i.toString()
-            numbers.forEachIndexed { index2, j ->
-                if (index != index2) {
-                    println("str 1 = $str")
-                    str += j.toString()
-                    list.add(str)
-                    println("str 2 = $str")
-                    str = i.toString()
-                }
-            }
-        }
-        list.forEach { print("$it ") }
-        println()
-        list.sortedDescending().forEach { print("$it ") }
-        println()
-        answer = list.sortedDescending()[0]
-
-        return answer
-    }
->>>>>>> c442bba... Add sort algorithm
 }
