8de869623c46086062b0c47c31e026a017b3e2e9
JC-Choo <jicheolchoo@gmail.com>
2021-01-24 23:24:04 +0900

    Modify package
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- data_structure/src/test/java/dev/chu/data_structure/ExampleUnitTest.kt
+++ data_structure/src/test/java/dev/chu/data_structure/ExampleUnitTest.kt
@@ -3,9 +3,7 @@
 import dev.chu.data_structure.list.DS_LinkedList
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
+            /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
