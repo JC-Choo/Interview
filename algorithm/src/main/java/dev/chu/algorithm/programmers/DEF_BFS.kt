1b4de1bd2f6ec695a7b47f4e0509fbab94165cc7
JC-Choo <jicheolchoo@gmail.com>
2021-02-15 21:55:58 +0900

    test
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

---
+++ algorithm/src/main/java/dev/chu/algorithm/programmers/DEF_BFS.kt
@@ -0,0 +1,17 @@
+package dev.chu.algorithm.programmers
+
+/**
+ * 깊이/너비 우선 탐색 (DFS/BFS)
+ */
+object DEF_BFS {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("타켓_넘버 01 = "+타켓_넘버(intArrayOf(1, 1, 1, 1, 1), 3))
+    }
+
+    fun 타켓_넘버(numbers: IntArray, target: Int): Int {
+        var answer = 0
+
+        return answer
+    }
+}
