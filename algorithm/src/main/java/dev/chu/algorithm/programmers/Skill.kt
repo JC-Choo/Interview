f45ac2dd31d99ede99968c7cc3142fc98fde7942
JC-Choo <jicheolchoo@gmail.com>
2021-02-18 21:52:17 +0900

    소수찾기, 조이스틱
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- algorithm/src/main/java/dev/chu/algorithm/programmers/Skill.kt
+++ algorithm/src/main/java/dev/chu/algorithm/programmers/Skill.kt
@@ -18,11 +18,135 @@ object Skill {
 //        println(스킬체크테스트_레벨_2_문제_1("4177252841", 2)) // 77252841
 //        println(스킬체크테스트_레벨_2_문제_1("4177252841", 3)) // 7752841
 //        println(스킬체크테스트_레벨_2_문제_1("4177252841", 4)) // 775841
-        println(스킬체크테스트_레벨_2_문제_1("4177252841", 5)) // 77841
+//        println(스킬체크테스트_레벨_2_문제_1("4177252841", 5)) // 77841
 
 //        println(스킬체크테스트_레벨_2_문제_2("110010101001").forEach { print("$it ") })  // 3, 8
 //        println(스킬체크테스트_레벨_2_문제_2("01110").forEach { print("$it ") }) // 3, 3
 //        println(스킬체크테스트_레벨_2_문제_2("1111111").forEach { print("$it ") })   // 4, 1
+
+//        println(스킬테스트_레벨_2_조이스틱("JAZ"))     // 11
+//        println(스킬테스트_레벨_2_조이스틱("JEROEN"))  // 56
+//        println(스킬테스트_레벨_2_조이스틱("JAN"))  // 23
+
+        println(스킬테스트_레벨_2_소수("17"))    // 3 -> 7 17 71
+        println(스킬테스트_레벨_2_소수("011"))   // 2 -> 11 101
+    }
+
+    fun 스킬테스트_레벨_2_소수(numbers: String): Int {
+        if (numbers.length == 1) {
+            return if (numbers == "2" || numbers == "3" || numbers == "5" || numbers == "7") 1 else 0
+        }
+
+        var answer = 0
+        val max = numbers.toCharArray().sortedArrayDescending().joinToString("").toInt()
+        val sqrtOfMax = Math.sqrt(max.toDouble()).toInt()
+
+        val list = mutableListOf(0, 0)
+        for (i in 2 .. max) {
+            list.add(i)
+        }
+
+        for (i in 2 .. sqrtOfMax) {
+            if (list[i] == 0) continue
+
+            var j = 2*i
+            while (j <= max) {
+                list[j] = 0
+                j += i
+            }
+        }
+
+        val filterList = list.filter { it != 0 }.map { it.toString() }.toMutableList()
+
+        numbers.forEach { c ->
+            filterList.forEachIndexed { index2, s ->
+                if (s.contains(c)) {
+                    filterList[index2] = s.replaceFirst(c.toString(), "0")
+                }
+            }
+        }
+
+        answer = filterList.filter { it.toInt() == 0 }.size
+
+        return answer
+    }
+
+    fun 스킬테스트_레벨_2_조이스틱(name: String): Int {
+        var answer = 0
+        /**
+         * 위로 : 다음 알파벳
+         * 아래로 : 이전 알파벳(A -> Z)
+         * 왼쪽 : 커서를 왼쪽으로 이동(첫번째 위치에서 왼쪽으로 이동하면 마지막에 커서)
+         * 오른쪽 : 커서를 오른쪽으로 이동
+         *
+         * 풀이 방법 :
+         * 1. 첫 번째는 무조건 up or down 해서 작은 수
+         * 2. 두 번째 부터는 right or left 해서 작은 수 구한 다음, 다시 1번
+         */
+        val alphas = mutableListOf<String>()
+        for (i in 65 .. 90) {
+            alphas.add(i.toChar().toString())
+        }
+
+//        println("up = "+up(name[0].toString(), alphas))
+//        println("down = "+down(name[name.length-1].toString(), alphas))
+        val midCount = name.length/2
+        var rightCount = 0
+        var leftCount = 0
+        name.forEachIndexed { index, c ->
+            when {
+                index == 0 -> {
+                    val a = upOrDown(c.toString(), alphas)
+                    answer += a
+                    println("a = $a")
+                }
+                index <= midCount -> {
+                    if (c == 'A') return@forEachIndexed
+                    rightCount++
+                    answer++
+                    val a = upOrDown(c.toString(), alphas)
+                    answer += a
+                    println("rightCount = $rightCount. a = $a")
+                }
+                else -> {
+                    if (c == 'A') return@forEachIndexed
+                    leftCount++
+                    answer++
+                    val b = upOrDown(c.toString(), alphas)
+                    answer += b
+                    println("leftCount = $leftCount. b = $b")
+                }
+            }
+        }
+
+        return answer
+    }
+
+    private fun upOrDown(str: String, alphas: MutableList<String>): Int {
+        val upCount = up(str, alphas)
+        val downCount = down(str, alphas)
+
+        return if (upCount >= downCount) downCount else upCount
+    }
+    private fun up(str: String, alphas: MutableList<String>): Int {
+        var count = 0
+        alphas.forEach {
+            if (it == str) {
+                return count
+            }
+            count ++
+        }
+        return count
+    }
+    private fun down(str: String, alphas: MutableList<String>): Int {
+        var count = 0
+        for (i in alphas.size-1 downTo 0) {
+            count ++
+            if (alphas[i] == str) {
+                return count
+            }
+        }
+        return count
     }
 
     fun 스킬체크테스트_레벨_2_문제_2(s: String): IntArray {
